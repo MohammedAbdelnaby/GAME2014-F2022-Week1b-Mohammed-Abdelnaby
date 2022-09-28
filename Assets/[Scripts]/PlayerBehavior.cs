0b1fa6fbdf618a92123551ea5411815a63c704d7
Mohammed <mohammed.abdelnaby@georgebrown.ca>
2022-09-28 14:07:01 -0400

    added simple touch input

--- Assets/[Scripts]/PlayerBehavior.cs
+++ Assets/[Scripts]/PlayerBehavior.cs
@@ -7,19 +7,39 @@ public class PlayerBehavior : MonoBehaviour
     public float speed = 2.0f;
     public Boundray boundray;
     public float VerticalPostions;
+
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
     // Update is called once per frame
     void Update()
     {
+        //ConventionalInput();
+        MobileInput();
         Move();
     }
 
     public void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-
-        transform.position += new Vector3(x, 0.0f, 0.0f);
-
         var clampedPostion = Mathf.Clamp(transform.position.x, boundray.min, boundray.max);
         transform.position = new Vector2(clampedPostion, VerticalPostions);
     }   
+
+    public void ConventionalInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+
+        transform.position += new Vector3(x, 0.0f, 0.0f);
+    }
+
+    public void MobileInput()
+    {
+        foreach (var touch in Input.touches)
+        {
+            transform.position = camera.ScreenToWorldPoint(touch.position);
+        }
+    }
 }
